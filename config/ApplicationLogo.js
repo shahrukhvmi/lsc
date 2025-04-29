4bc17ae4cf0d9b8de36a646125dd968fd6aa03f1
shahrukhvmi <shahrukh.vmi@gmail.com>
2025-04-29 12:30:38 +0500

    update

--- config/ApplicationLogo.js
+++ config/ApplicationLogo.js
@@ -5,7 +5,6 @@ import Image from "next/image";
 const ApplicationLogo = ({ logoUrl, priority = false, ...props }) => {
   const logoSrc = logoUrl || defaultLogo;
 
-  
   return (
     <Image
       src={logoSrc}
